1b500d9ccdb30a49c5e7fcd03dafd9bfa79a9512
RajatNSharma <103058654+RajatNSharma@users.noreply.github.com>
2022-05-17 15:37:48 +0530

    Add files via upload

---
+++ Bonus calculation example.py
@@ -0,0 +1,16 @@
+ch= input("Gender of the employee M or F")
+sal= int(input("Salary of the employee"))
+if(ch=='M' and sal>10000):
+    bonus = 0.05*sal
+    print("a =",bonus)
+elif(ch=='F' and sal>10000):
+    bonus = 0.10*sal
+    print("a =",bonus)
+else:
+    (sal<=10000)
+    bonus = 0.02*sal
+    print("a =", bonus)
+
+
+
+
