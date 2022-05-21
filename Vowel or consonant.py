a0d2a1b884a8d7d8cec60ba9d3cbb8367365eeda
RajatNSharma <103058654+RajatNSharma@users.noreply.github.com>
2022-05-21 19:06:23 +0530

    Add files via upload

---
+++ Vowel or consonant.py
@@ -0,0 +1,7 @@
+ch= input("Enter any value between a and z :")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("vowel")
+elif(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("vowel")
+else:
+    print("consonant")
